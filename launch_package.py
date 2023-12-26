fcda73897766a4942fcde0d024d492ec11251a8a
Giuseppe7887 <guseppegravante078@gmail.com>
2023-12-26 21:39:39 +0100

    build pip branch

---
+++ launch_package.py
@@ -0,0 +1,19 @@
+import os
+from pick import pick
+
+project_dir = os.listdir("adb_connector_python/dist")
+
+
+clear = [el for el in project_dir]
+clear.append("auto")
+
+scelta = pick(options=clear,title="scegli una versione",indicator=">")[0]
+
+CMD = "twine upload adb_connector_python/dist/{0} -u {1} -p {2}"
+
+with open("credeziali","r") as credenziali:
+    user, token = credenziali.readlines()
+    if scelta == "auto":
+        os.system(CMD.format("*",user.strip(),token.strip()))        
+    else:
+        os.system(CMD.format(scelta.strip(),user.strip(),token.strip()))
