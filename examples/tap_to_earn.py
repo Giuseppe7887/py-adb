6744f4945646379f4afe37b3afee5c9cfcf32b19
Giuseppe7887 <guseppegravante078@gmail.com>
2024-04-10 14:59:08 +0200

    add examples

--- examples/tap_to_earn.py
+++ examples/tap_to_earn.py
@@ -3,4 +3,8 @@ from adb_connector_python.connector import Py_adb
 
 device = Py_adb()
 
-device.multitap(times=0,x=700,y=1600)
+device.multitap(
+    times=0, # set to 0 to tap forever
+    x=700,
+    y=1600
+    )
